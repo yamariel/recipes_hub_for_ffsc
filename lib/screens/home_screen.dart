d169bf483077845db53b2bdf2bc43c93cd1d5533
yamariel <ariel.yamien@epitech.eu>
2026-08-12 20:20:14 +0000

    [UPDATE]: amélioration de l'affichage

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,52 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:recipes_hub/widgets/recipe_card.dart';
 import '../data/recipe_data.dart';
-import '../widgets/recipe_card.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final popularRecipes = recipes.take(3);
     return Scaffold(
       appBar: AppBar(
-        title: Text("Recipes Hub"),
+        title: Text("RecipeHub",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
         actions: [
           IconButton(
-              onPressed: () {},
+            onPressed: () {}, 
               icon: Icon(Icons.dark_mode)
           )
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              "Que voulez-vous cuisiner ? ",
-              style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Bienvenue sur Recipe Hub !',
+                style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            const SizedBox(height: 20),
-            const Text('Recettes populaires',
+              const SizedBox(height: 10),
+              Text(
+                'Découvrez de délicieuses recettes et trouvez '
+                'l’inspiration pour vos prochains repas.',
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+              const SizedBox(height: 20),
+              Wrap(
+                spacing: 15,
+                runSpacing: 10,
+                children: [
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      context.push('/recipes');
+                    },
+                    label: const Text('Voir les recettes'),
+                    icon: const Icon(Icons.restaurant_menu),
+                  ),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      context.push('/add-recipe');
+                    },
+                    label: const Text('Ajouter une recette'),
+                    icon: const Icon(Icons.add),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Text(
+                'Top 3 des recettes populaires',
                 style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
                 )
-            ),
-            const SizedBox(height: 12),
-            Expanded(
-              child: ListView.builder(
-                itemCount: recipes.length,
-                itemBuilder: (context, index) {
-                  final recipe = recipes[index];
-                  return RecipeCard(recipe: recipe);
-                },
               ),
-            ),
-          ],
+              const SizedBox(height: 10),
+              ...popularRecipes.map(
+                (recipe) => Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: RecipeCard(
+                      recipe: recipe,
+                    onTap: () {
+                        context.push('/recipe', extra: recipe);
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
