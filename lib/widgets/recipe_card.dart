442924fc7ff002891fabe1fc8216a2833e1aa286
yamariel <ariel.yamien@epitech.eu>
2026-08-12 20:02:42 +0000

    [UPDATE]: ajout de detection de geste

--- lib/widgets/recipe_card.dart
+++ lib/widgets/recipe_card.dart
@@ -3,75 +3,82 @@ import '../models/recipe.dart';
 
 class RecipeCard extends StatelessWidget {
   final Recipe recipe;
-  const RecipeCard({super.key, required this.recipe});
+  final VoidCallback? onTap;
+  const RecipeCard({super.key, required this.recipe, this.onTap});
 
   @override
   Widget build(BuildContext context) {
     return Card(
       clipBehavior: Clip.antiAlias,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-            flex: 5,
-            child: Image.network(
-              recipe.image,
-              width: double.infinity,
-              fit: BoxFit.cover,
-            ),
-          ),
+      child: InkWell(
+        onTap: onTap,
+        child: SizedBox(
+          height: 300,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                flex: 5,
+                child: Image.network(
+                  recipe.image,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+              ),
 
-          Expanded(
-            flex: 4,
-            child: Padding(
-              padding: const EdgeInsets.all(12),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    recipe.title,
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    style: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-
-                  const SizedBox(height: 6),
-
-                  Expanded(
-                    child: Text(
-                      recipe.description,
-                      maxLines: 3,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-
-                  Row(
+              Expanded(
+                flex: 4,
+                child: Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      const Icon(Icons.timer, size: 16),
-                      const SizedBox(width: 4),
-                      Text('${recipe.preparationTime} min'),
+                      Text(
+                        recipe.title,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
 
-                      const Spacer(),
+                      const SizedBox(height: 6),
 
-                      const Icon(Icons.category, size: 16),
-                      const SizedBox(width: 4),
-                      Flexible(
+                      Expanded(
                         child: Text(
-                          recipe.category,
-                          maxLines: 1,
+                          recipe.description,
+                          maxLines: 3,
                           overflow: TextOverflow.ellipsis,
                         ),
                       ),
+
+                      Row(
+                        children: [
+                          const Icon(Icons.timer, size: 16),
+                          const SizedBox(width: 4),
+                          Text('${recipe.preparationTime} min'),
+
+                          const Spacer( flex: 4,),
+
+                          const Icon(Icons.category, size: 16),
+                          const SizedBox(width: 4),
+                          Flexible(
+                            child: Text(
+                              recipe.category,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ],
+                      ),
                     ],
                   ),
-                ],
+                ),
               ),
-            ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
