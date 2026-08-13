bf051aed3823ac9a5fb451ade0704da1a806274b
yamariel <ariel.yamien@epitech.eu>
2026-08-13 10:24:13 +0000

    [UPDATE]: Adaptation de la grid en fonction de la taille d'écran et utilisation du service de filtre

--- lib/screens/recipes_screen.dart
+++ lib/screens/recipes_screen.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import '../data/recipe_data.dart';
 import '../models/recipe.dart';
+import '../services/recipe_filter.dart';
 import '../widgets/recipe_card.dart';
 import '../widgets/search_bar.dart';
 import '../widgets/category_chip.dart';
 
-class RecipesScreen extends StatefulWidget{
+class RecipesScreen extends StatefulWidget {
   const RecipesScreen({super.key});
 
   @override
@@ -18,27 +19,29 @@ class _RecipesScreenState extends State<RecipesScreen> {
   String selectedCategory = 'Toutes';
 
   List<Recipe> get filteredRecipes {
-    return recipes.where((recipe) {
-      final matchesSearch = recipe.title
-          .toLowerCase()
-          .contains(searchQuery.toLowerCase());
-
-      final matchesCategory = selectedCategory == 'Toutes' ||
-          recipe.category == selectedCategory;
-
-      return matchesSearch && matchesCategory;
-    }).toList();
+    final filter = RecipeFilter();
+    return filter.filter(
+      recipes: recipes,
+      searchQuery: searchQuery,
+      selectedCategory: selectedCategory,
+    );
   }
+
   @override
   Widget build(BuildContext context) {
-    final categories = ['Toutes',
-      ...recipes.map((recipe) => recipe.category).toSet()
+    final categories = [
+      'Toutes',
+      ...recipes.map((recipe) => recipe.category).toSet(),
     ];
+    final width = MediaQuery.of(context).size.width;
+    final crossAxisCount = width >= 900
+        ? 4
+        : width >= 600
+        ? 3
+        : 2;
 
     return Scaffold(
-      appBar: AppBar(
-          title: Text("Toutes les recettes"),
-      ),
+      appBar: AppBar(title: Text("Toutes les recettes")),
       body: Column(
         children: [
           Padding(
@@ -54,48 +57,46 @@ class _RecipesScreenState extends State<RecipesScreen> {
           SizedBox(
             height: 45,
             child: ListView.builder(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                scrollDirection: Axis.horizontal,
-                itemCount: categories.length,
-                itemBuilder: (context, index) {
-                  final category = categories[index];
-                  return CategoryChip(
-                    label: category,
-                    selected: category == selectedCategory,
-                    onSelected: () {
-                      setState(() {
-                        selectedCategory = category;
-                      });
-                    }
-                  );
-                }
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              scrollDirection: Axis.horizontal,
+              itemCount: categories.length,
+              itemBuilder: (context, index) {
+                final category = categories[index];
+                return CategoryChip(
+                  label: category,
+                  selected: category == selectedCategory,
+                  onSelected: () {
+                    setState(() {
+                      selectedCategory = category;
+                    });
+                  },
+                );
+              },
             ),
           ),
           const SizedBox(height: 16),
           Expanded(
-              child:
-              filteredRecipes.isEmpty ?
-              const Center(
-                child: Text("'Aucune recette trouvée"),
-              ) : GridView.builder(
-                  padding: const EdgeInsets.all(16),
-                  itemCount: filteredRecipes.length,
-                  gridDelegate:  const SliverGridDelegateWithMaxCrossAxisExtent(
-                    maxCrossAxisExtent: 300,
-                    childAspectRatio: 0.75,
-                    crossAxisSpacing: 24,
-                    mainAxisSpacing: 30
-                  ),
-                  itemBuilder: (context, index) {
-                    final recipe =  filteredRecipes[index];
-                    return RecipeCard(
+            child: filteredRecipes.isEmpty
+                ? const Center(child: Text("'Aucune recette trouvée"))
+                : GridView.builder(
+                    padding: const EdgeInsets.all(16),
+                    itemCount: filteredRecipes.length,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: crossAxisCount,
+                      childAspectRatio: 0.75,
+                      crossAxisSpacing: 24,
+                      mainAxisSpacing: 30,
+                    ),
+                    itemBuilder: (context, index) {
+                      final recipe = filteredRecipes[index];
+                      return RecipeCard(
                         recipe: recipe,
                         onTap: () {
                           context.push('/recipe', extra: recipe);
-                        }
-                    );
-                  }
-              )
+                        },
+                      );
+                    },
+                  ),
           ),
         ],
       ),
