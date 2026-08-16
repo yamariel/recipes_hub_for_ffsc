c9cbfb84fccabbab573f8339d18b585f3d9d24c0
yamariel <ariel.yamien@epitech.eu>
2026-08-16 08:34:02 +0000

    [FIX]: correction de l'affichage en fonction de l'écran

--- lib/screens/recipe_detail_screen.dart
+++ lib/screens/recipe_detail_screen.dart
@@ -10,79 +10,87 @@ class RecipeDetailScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text(recipe.title)),
       body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image.network(
-                recipe.image,
-                width: double.infinity,
-                height: 280,
-                fit: BoxFit.cover,
-                errorBuilder: (context, error, stackTrace) => const Center(child: Icon(Icons.error)),
-            ),
-            Padding(
+        child: Center(
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(maxWidth: 900),
+            child: Padding(
               padding: const EdgeInsets.all(16),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    recipe.title,
-                    style: TextStyle(
-                      fontSize: 26,
-                      fontWeight: FontWeight.bold,
-                    ),
+                  Image.network(
+                      recipe.image,
+                      width: double.infinity,
+                      height: 280,
+                      fit: BoxFit.cover,
+                      errorBuilder: (context, error, stackTrace) => const Center(child: Icon(Icons.error)),
                   ),
-                  const SizedBox(height: 12),
-                  Row(
-                    children: [
-                      const Icon(Icons.timer, size: 16),
-                      const SizedBox(width: 4),
-                      Text('${recipe.preparationTime} min'),
-                      const Spacer(),
-                      const Icon(Icons.category, size: 16),
-                      const SizedBox(width: 4),
-                      Text(recipe.category),
-                    ],
-                  ),
-                  const SizedBox(height: 16),
-                  Wrap(
-                    children: [
-                      Text(recipe.description, style: TextStyle(fontSize: 16)),
-                    ],
-                  ),
-                  const SizedBox(height: 20),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("Ingrédients:",
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      for (final ingredient in recipe.ingredients)
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Row(
-                            children: [
-                              const Icon(
-                                  Icons.published_with_changes_rounded
-                              ),
-                              const SizedBox(width: 4,),
-                              Text(
-                                  ingredient,
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                  )
-                              ),
-                            ],
+                  Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Column(
+                      children: [
+                        Text(
+                          recipe.title,
+                          style: TextStyle(
+                            fontSize: 26,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
-                    ],
+                        const SizedBox(height: 12),
+                        Row(
+                          children: [
+                            const Icon(Icons.timer, size: 16),
+                            const SizedBox(width: 4),
+                            Text('${recipe.preparationTime} min'),
+                            const Spacer(),
+                            const Icon(Icons.category, size: 16),
+                            const SizedBox(width: 4),
+                            Text(recipe.category),
+                          ],
+                        ),
+                        const SizedBox(height: 16),
+                        Wrap(
+                          children: [
+                            Text(recipe.description, style: TextStyle(fontSize: 16)),
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Ingrédients:",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            for (final ingredient in recipe.ingredients)
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Row(
+                                  children: [
+                                    const Icon(
+                                        Icons.published_with_changes_rounded
+                                    ),
+                                    const SizedBox(width: 4,),
+                                    Text(
+                                        ingredient,
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                        )
+                                    ),
+                                  ],
+                                ),
+                              ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
             ),
-          ],
+          ),
         ),
       ),
     );
